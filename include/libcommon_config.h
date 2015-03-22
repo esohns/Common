b121c7e74d18df7456cc42511b874c6ba651fbe4
Erik Sohns <eriksohns@123mail.org>
2015-03-22 04:03:23 +0000

    minor changes

--- include/libcommon_config.h
+++ include/libcommon_config.h
@@ -1,13 +1,23 @@
 #ifndef _LIBCOMMON_CONFIG_H
 #define _LIBCOMMON_CONFIG_H 1
  
-/* libCommon-config.h. Generated automatically at end of configure. */
+/* libcommon_config.h. Generated automatically at end of configure. */
 /* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #ifndef LIBCOMMON_HAVE_DLFCN_H
 #define LIBCOMMON_HAVE_DLFCN_H 1
+#endif
+
+/* Define to 1 if you have the `getcwd' function. */
+#ifndef LIBCOMMON_HAVE_GETCWD
+#define LIBCOMMON_HAVE_GETCWD 1
+#endif
+
+/* Define to 1 if you have the `gettimeofday' function. */
+#ifndef LIBCOMMON_HAVE_GETTIMEOFDAY
+#define LIBCOMMON_HAVE_GETTIMEOFDAY 1
 #endif
 
 /* Define to 1 if you have the <inttypes.h> header file. */
@@ -25,6 +35,21 @@
 #define LIBCOMMON_HAVE_MEMORY_H 1
 #endif
 
+/* Define to 1 if you have the `memset' function. */
+#ifndef LIBCOMMON_HAVE_MEMSET
+#define LIBCOMMON_HAVE_MEMSET 1
+#endif
+
+/* Define to 1 if you have the `mkdir' function. */
+#ifndef LIBCOMMON_HAVE_MKDIR
+#define LIBCOMMON_HAVE_MKDIR 1
+#endif
+
+/* Define to 1 if you have the `realpath' function. */
+#ifndef LIBCOMMON_HAVE_REALPATH
+#define LIBCOMMON_HAVE_REALPATH 1
+#endif
+
 /* Define to 1 if you have the <stdint.h> header file. */
 #ifndef LIBCOMMON_HAVE_STDINT_H
 #define LIBCOMMON_HAVE_STDINT_H 1
@@ -33,6 +58,11 @@
 /* Define to 1 if you have the <stdlib.h> header file. */
 #ifndef LIBCOMMON_HAVE_STDLIB_H
 #define LIBCOMMON_HAVE_STDLIB_H 1
+#endif
+
+/* Define to 1 if you have the `strerror' function. */
+#ifndef LIBCOMMON_HAVE_STRERROR
+#define LIBCOMMON_HAVE_STRERROR 1
 #endif
 
 /* Define to 1 if you have the <strings.h> header file. */
@@ -55,9 +85,22 @@
 #define LIBCOMMON_HAVE_SYS_TYPES_H 1
 #endif
 
+/* Define to 1 if you have the `uname' function. */
+#ifndef LIBCOMMON_HAVE_UNAME
+#define LIBCOMMON_HAVE_UNAME 1
+#endif
+
 /* Define to 1 if you have the <unistd.h> header file. */
 #ifndef LIBCOMMON_HAVE_UNISTD_H
 #define LIBCOMMON_HAVE_UNISTD_H 1
+#endif
+
+/* Define if a version suffix is present. */
+/* #undef HAVE_VERSION_DEVEL */
+
+/* Define to 1 if the system has the type `_Bool'. */
+#ifndef LIBCOMMON_HAVE__BOOL
+#define LIBCOMMON_HAVE__BOOL 1
 #endif
 
 /* Define to the sub-directory in which libtool stores uninstalled libraries.
@@ -88,7 +131,7 @@
 
 /* Define to the full name and version of this package. */
 #ifndef LIBCOMMON_PACKAGE_STRING
-#define LIBCOMMON_PACKAGE_STRING "libCommon 0.1"
+#define LIBCOMMON_PACKAGE_STRING "libCommon 0.0.1-devel"
 #endif
 
 /* Define to the one symbol short name of this package. */
@@ -103,16 +146,27 @@
 
 /* Define to the version of this package. */
 #ifndef LIBCOMMON_PACKAGE_VERSION
-#define LIBCOMMON_PACKAGE_VERSION "0.1"
+#define LIBCOMMON_PACKAGE_VERSION "0.0.1-devel"
 #endif
 
 /* Define to 1 if you have the ANSI C header files. */
-/* #undef STDC_HEADERS */
+#ifndef LIBCOMMON_STDC_HEADERS
+#define LIBCOMMON_STDC_HEADERS 1
+#endif
 
 /* Version number of package */
 #ifndef LIBCOMMON_VERSION
-#define LIBCOMMON_VERSION "0.1"
+#define LIBCOMMON_VERSION "0.0.1-devel"
 #endif
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef gid_t */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef uid_t */
  
 /* once: _LIBCOMMON_CONFIG_H */
 #endif
