30644a9b43e06dd662799cebca47317525b8d500
Erik Sohns <eriksohns@123mail.org>
2017-11-16 19:15:04 +0100

    minor changes

--- include/libCommon_config.h
+++ include/libCommon_config.h
@@ -1,172 +1,36 @@
-#ifndef _LIBCOMMON_CONFIG_H
-#define _LIBCOMMON_CONFIG_H 1
- 
-/* libcommon_config.h. Generated automatically at end of configure. */
-/* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.ac by autoheader.  */
+/* config.h.in.cmake */
 
-/* Define to 1 if you have the <dlfcn.h> header file. */
-#ifndef LIBCOMMON_HAVE_DLFCN_H
-#define LIBCOMMON_HAVE_DLFCN_H 1
-#endif
+#define libCommon_VERSION_MAJOR 0
+#define libCommon_VERSION_MINOR 0
+#define libCommon_VERSION_MICRO 1
+#define libCommon_VERSION_DEVEL devel
 
-/* Define to 1 if you have the `getcwd' function. */
-#ifndef LIBCOMMON_HAVE_GETCWD
-#define LIBCOMMON_HAVE_GETCWD 1
-#endif
+#define LIBCOMMON_PACKAGE "Common"
 
-/* Define to 1 if you have the `gettimeofday' function. */
-#ifndef LIBCOMMON_HAVE_GETTIMEOFDAY
-#define LIBCOMMON_HAVE_GETTIMEOFDAY 1
-#endif
-
-/* Define to 1 if you have the <inttypes.h> header file. */
-#ifndef LIBCOMMON_HAVE_INTTYPES_H
-#define LIBCOMMON_HAVE_INTTYPES_H 1
-#endif
-
-/* Define to 1 if you have the `ACE' library (-lACE). */
-#ifndef LIBCOMMON_HAVE_LIBACE
-#define LIBCOMMON_HAVE_LIBACE 1
-#endif
-
-/* Define to 1 if you have the <memory.h> header file. */
-#ifndef LIBCOMMON_HAVE_MEMORY_H
-#define LIBCOMMON_HAVE_MEMORY_H 1
-#endif
-
-/* Define to 1 if you have the `memset' function. */
-#ifndef LIBCOMMON_HAVE_MEMSET
-#define LIBCOMMON_HAVE_MEMSET 1
-#endif
-
-/* Define to 1 if you have the `mkdir' function. */
-#ifndef LIBCOMMON_HAVE_MKDIR
-#define LIBCOMMON_HAVE_MKDIR 1
-#endif
-
-/* Define to 1 if you have the `realpath' function. */
-#ifndef LIBCOMMON_HAVE_REALPATH
-#define LIBCOMMON_HAVE_REALPATH 1
-#endif
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#ifndef LIBCOMMON_HAVE_STDINT_H
-#define LIBCOMMON_HAVE_STDINT_H 1
-#endif
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#ifndef LIBCOMMON_HAVE_STDLIB_H
-#define LIBCOMMON_HAVE_STDLIB_H 1
-#endif
-
-/* Define to 1 if you have the `strerror' function. */
-#ifndef LIBCOMMON_HAVE_STRERROR
-#define LIBCOMMON_HAVE_STRERROR 1
-#endif
-
-/* Define to 1 if you have the <strings.h> header file. */
-#ifndef LIBCOMMON_HAVE_STRINGS_H
-#define LIBCOMMON_HAVE_STRINGS_H 1
-#endif
-
-/* Define to 1 if you have the <string.h> header file. */
-#ifndef LIBCOMMON_HAVE_STRING_H
-#define LIBCOMMON_HAVE_STRING_H 1
-#endif
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#ifndef LIBCOMMON_HAVE_SYS_STAT_H
-#define LIBCOMMON_HAVE_SYS_STAT_H 1
-#endif
-
-/* Define to 1 if you have the <sys/types.h> header file. */
-#ifndef LIBCOMMON_HAVE_SYS_TYPES_H
-#define LIBCOMMON_HAVE_SYS_TYPES_H 1
-#endif
-
-/* Define to 1 if you have the `uname' function. */
-#ifndef LIBCOMMON_HAVE_UNAME
-#define LIBCOMMON_HAVE_UNAME 1
-#endif
-
-/* Define to 1 if you have the <unistd.h> header file. */
-#ifndef LIBCOMMON_HAVE_UNISTD_H
-#define LIBCOMMON_HAVE_UNISTD_H 1
-#endif
-
-/* Define if a version suffix is present. */
-/* #undef HAVE_VERSION_DEVEL */
-
-/* Define to 1 if the system has the type `_Bool'. */
-#ifndef LIBCOMMON_HAVE__BOOL
-#define LIBCOMMON_HAVE__BOOL 1
-#endif
-
-/* Define to the sub-directory in which libtool stores uninstalled libraries.
-   */
-#ifndef LIBCOMMON_LT_OBJDIR
-#define LIBCOMMON_LT_OBJDIR ".libs/"
-#endif
-
-/* meta-package-name */
-#ifndef LIBCOMMON_META_PACKAGE_NAME
-#define LIBCOMMON_META_PACKAGE_NAME "libCommon"
-#endif
+/* #undef VALGRIND_SUPPORT */
 
 /* Name of package */
-#ifndef LIBCOMMON_PACKAGE
-#define LIBCOMMON_PACKAGE "libCommon"
-#endif
+/* #undef  */
+//#undef PACKAGE
 
 /* Define to the address where bug reports for this package should be sent. */
-#ifndef LIBCOMMON_PACKAGE_BUGREPORT
-#define LIBCOMMON_PACKAGE_BUGREPORT "eriksohns@123mail.org"
-#endif
+//#undef PACKAGE_BUGREPORT
 
 /* Define to the full name of this package. */
-#ifndef LIBCOMMON_PACKAGE_NAME
-#define LIBCOMMON_PACKAGE_NAME "libCommon"
-#endif
+//#undef PACKAGE_NAME
 
 /* Define to the full name and version of this package. */
-#ifndef LIBCOMMON_PACKAGE_STRING
-#define LIBCOMMON_PACKAGE_STRING "libCommon 0.0.1-devel"
-#endif
+//#undef PACKAGE_STRING
 
 /* Define to the one symbol short name of this package. */
-#ifndef LIBCOMMON_PACKAGE_TARNAME
-#define LIBCOMMON_PACKAGE_TARNAME "libCommon"
-#endif
+//#undef PACKAGE_TARNAME
 
 /* Define to the home page for this package. */
-#ifndef LIBCOMMON_PACKAGE_URL
-#define LIBCOMMON_PACKAGE_URL "http://www.github.com/esohns/libCommon"
-#endif
+//#undef PACKAGE_URL
 
 /* Define to the version of this package. */
-#ifndef LIBCOMMON_PACKAGE_VERSION
-#define LIBCOMMON_PACKAGE_VERSION "0.0.1-devel"
-#endif
-
-/* Define to 1 if you have the ANSI C header files. */
-#ifndef LIBCOMMON_STDC_HEADERS
-#define LIBCOMMON_STDC_HEADERS 1
-#endif
+//#undef PACKAGE_VERSION
 
 /* Version number of package */
-#ifndef LIBCOMMON_VERSION
-#define LIBCOMMON_VERSION "0.0.1-devel"
-#endif
+//#undef VERSION
 
-/* Define to `int' if <sys/types.h> doesn't define. */
-/* #undef gid_t */
-
-/* Define to `unsigned int' if <sys/types.h> does not define. */
-/* #undef size_t */
-
-/* Define to `int' if <sys/types.h> doesn't define. */
-/* #undef uid_t */
- 
-/* once: _LIBCOMMON_CONFIG_H */
-#endif
--- src/math/CMakeFiles/CommonMath.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CommonMath.
-# This may be replaced when dependencies are built.
