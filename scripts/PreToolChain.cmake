761a3014dc3303b667f4d926756ad5dbd258973b
Erik Sohns <erik.sohns@posteo.de>
2021-06-12 20:40:37 +0200

    minor changes

--- scripts/PreToolChain.cmake
+++ scripts/PreToolChain.cmake
@@ -47,16 +47,16 @@ elseif (WIN32)
 #  endforeach ()
   set (MSVC_RELEASE ${CMAKE_MATCH_1})
   set (MSVC_EDITION ${CMAKE_MATCH_2})
-  else ()
-   message (FATAL_ERROR "invalid/unknown MSVC generator (was: \"${CMAKE_GENERATOR}\")")
-  endif ()
+ else ()
+  message (FATAL_ERROR "invalid/unknown MSVC generator (was: \"${CMAKE_GENERATOR}\")")
+ endif ()
 #  message (STATUS "MSVC_VERSION: ${MSVC_VERSION}")
 #  message (STATUS "MSVC_RELEASE: ${MSVC_RELEASE}")
 #  message (STATUS "MSVC_EDITION: ${MSVC_EDITION}")
 
 #  if (MSVC_VERSION GREATER 1600) # MSVC 10 (*TODO*: inaccurate)
-  if (MSVC_RELEASE GREATER 10) # MSVC 10 (*TODO*: inaccurate)
-   set (MSVC_PATH_ROOT "C:/Program Files (x86)/Microsoft Visual Studio")
+ if (MSVC_RELEASE GREATER 10) # MSVC 10 (*TODO*: inaccurate)
+  set (MSVC_PATH_ROOT "C:/Program Files (x86)/Microsoft Visual Studio")
 #   set (MSVC_BIN_PATH "${MSVC_PATH_ROOT}/2017/Community/VC/Tools/MSVC/14.14.26428/bin/Hostx64/x86")
 #   set (MSVC_BIN_PATH "${MSVC_PATH_ROOT}/2017/Community/VC/Tools/MSVC/14.16.27023/bin/Hostx64/x86")
 #   set (MSVC_BIN_PATH "${MSVC_PATH_ROOT}/2019/Community/VC/Tools/MSVC/14.20.27508/bin/Hostx64/x86")
@@ -66,36 +66,35 @@ elseif (WIN32)
 #   set (MSVC_BIN_PATH "${MSVC_PATH_ROOT}/2019/Community/VC/Tools/MSVC/14.25.28610/bin/Hostx64/x86")
 #   set (MSVC_BIN_PATH "${MSVC_PATH_ROOT}/2019/Community/VC/Tools/MSVC/14.26.28801/bin/Hostx64/x86")
 #   set (MSVC_BIN_PATH "${MSVC_PATH_ROOT}/2019/Community/VC/Tools/MSVC/14.28.29333/bin/Hostx64/x86")
-   set (MSVC_BIN_PATH "${MSVC_PATH_ROOT}/2019/Community/VC/Tools/MSVC/14.28.29910/bin/Hostx64/x86")
+  set (MSVC_BIN_PATH "${MSVC_PATH_ROOT}/2019/Community/VC/Tools/MSVC/14.28.29910/bin/Hostx64/x86")
 #  elseif (MSVC_VERSION EQUAL 1600) # MSVC 10
-  elseif (MSVC_RELEASE EQUAL 10) # MSVC 10
+ elseif (MSVC_RELEASE EQUAL 10) # MSVC 10
 #   message (STATUS "MSVC_VERSION: ${MSVC_VERSION}")
-   set (MSVC_PATH_ROOT "C:/Program Files/Microsoft Visual Studio 10.0")
-   set (MSVC_BIN_PATH "${MSVC_PATH_ROOT}/VC/bin")
-  else ()
-   message (FATAL_ERROR "MSVC toolchain version (was: \"${MSVC_VERSION}\") not (yet) supported")
-  endif ()
-  find_program (COMPILER_PATH
-                ${COMPILER_EXE}
-                HINTS ${MSVC_BIN_PATH}
-                PATHS ${MSVC_BIN_PATH}
-#                PATH_SUFFIXES x64 x86
-                DOC "MSVC ${COMPILER_EXE}"
-                #NO_DEFAULT_PATH
-                #NO_CMAKE_ENVIRONMENT_PATH
-                #NO_CMAKE_PATH
-                #NO_SYSTEM_ENVIRONMENT_PATH
-                #NO_CMAKE_SYSTEM_PATH
-                #CMAKE_FIND_ROOT_PATH_BOTH |
-                #ONLY_CMAKE_FIND_ROOT_PATH |
-                #NO_CMAKE_FIND_ROOT_PATH
-               )
-  if (COMPILER_PATH)
+  set (MSVC_PATH_ROOT "C:/Program Files/Microsoft Visual Studio 10.0")
+  set (MSVC_BIN_PATH "${MSVC_PATH_ROOT}/VC/bin")
+ else ()
+  message (FATAL_ERROR "MSVC toolchain version (was: \"${MSVC_VERSION}\") not (yet) supported")
+ endif ()
+ find_program (COMPILER_PATH
+               ${COMPILER_EXE}
+               HINTS ${MSVC_BIN_PATH}
+               PATHS ${MSVC_BIN_PATH}
+#               PATH_SUFFIXES x64 x86
+               DOC "MSVC ${COMPILER_EXE}"
+               #NO_DEFAULT_PATH
+               #NO_CMAKE_ENVIRONMENT_PATH
+               #NO_CMAKE_PATH
+               #NO_SYSTEM_ENVIRONMENT_PATH
+               #NO_CMAKE_SYSTEM_PATH
+               #CMAKE_FIND_ROOT_PATH_BOTH |
+               #ONLY_CMAKE_FIND_ROOT_PATH |
+               #NO_CMAKE_FIND_ROOT_PATH
+              )
+ if (COMPILER_PATH)
 #   message (STATUS "found ${COMPILER_EXE}: ${COMPILER_PATH}")
-   set (MSVC_SUPPORT ON)
-  endif (COMPILER_PATH)
+  set (MSVC_SUPPORT ON)
+ endif (COMPILER_PATH)
  #elseif ()
  # message (FATAL_ERROR "toolchain not (yet) supported")
- endif ()
+ #endif ()
 endif ()
-
