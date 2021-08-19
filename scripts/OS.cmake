0d793b5218fcfd970f66dc26b8680659c6c40776
Erik Sohns <erik.sohns@posteo.de>
2021-08-19 11:16:27 +0200

    minor changes

--- scripts/OS.cmake
+++ scripts/OS.cmake
@@ -3,12 +3,12 @@
 function (get_linux_lsb_release_information)
  find_program (LSB_RELEASE_EXEC lsb_release)
  if (NOT LSB_RELEASE_EXEC)
-  message (FATAL_ERROR "Could not detect lsb_release executable, can not gather required information")
+  message (FATAL_ERROR "could not detect lsb_release executable, can not gather required information")
  endif (NOT LSB_RELEASE_EXEC)
 
- execute_process(COMMAND "${LSB_RELEASE_EXEC}" --short --id OUTPUT_VARIABLE LSB_RELEASE_ID_SHORT OUTPUT_STRIP_TRAILING_WHITESPACE)
- execute_process(COMMAND "${LSB_RELEASE_EXEC}" --short --release OUTPUT_VARIABLE LSB_RELEASE_VERSION_SHORT OUTPUT_STRIP_TRAILING_WHITESPACE)
- execute_process(COMMAND "${LSB_RELEASE_EXEC}" --short --codename OUTPUT_VARIABLE LSB_RELEASE_CODENAME_SHORT OUTPUT_STRIP_TRAILING_WHITESPACE)
+ execute_process (COMMAND "${LSB_RELEASE_EXEC}" --short --id OUTPUT_VARIABLE LSB_RELEASE_ID_SHORT OUTPUT_STRIP_TRAILING_WHITESPACE)
+ execute_process (COMMAND "${LSB_RELEASE_EXEC}" --short --release OUTPUT_VARIABLE LSB_RELEASE_VERSION_SHORT OUTPUT_STRIP_TRAILING_WHITESPACE)
+ execute_process (COMMAND "${LSB_RELEASE_EXEC}" --short --codename OUTPUT_VARIABLE LSB_RELEASE_CODENAME_SHORT OUTPUT_STRIP_TRAILING_WHITESPACE)
 
  set (LSB_RELEASE_ID_SHORT "${LSB_RELEASE_ID_SHORT}" PARENT_SCOPE)
  set (LSB_RELEASE_VERSION_SHORT "${LSB_RELEASE_VERSION_SHORT}" PARENT_SCOPE)
@@ -19,8 +19,8 @@ endfunction (get_linux_lsb_release_information)
 
 if (UNIX)
  if (CMAKE_SYSTEM_NAME MATCHES "Linux")
-  get_linux_lsb_release_information()
-  message(STATUS "Linux ${LSB_RELEASE_ID_SHORT} ${LSB_RELEASE_VERSION_SHORT} ${LSB_RELEASE_CODENAME_SHORT}")
+  get_linux_lsb_release_information ()
+  message (STATUS "Linux ${LSB_RELEASE_ID_SHORT} ${LSB_RELEASE_VERSION_SHORT} ${LSB_RELEASE_CODENAME_SHORT}")
  endif (CMAKE_SYSTEM_NAME MATCHES "Linux")
 endif (UNIX)
 
