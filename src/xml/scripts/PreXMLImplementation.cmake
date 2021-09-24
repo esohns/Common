4c57fb19ea7b707682b5d3f211ec7dbadf58077d
Erik Sohns <erik.sohns@posteo.de>
2021-09-24 20:38:53 +0200

    minor changes

--- src/xml/scripts/PreXMLImplementation.cmake
+++ src/xml/scripts/PreXMLImplementation.cmake
@@ -4,6 +4,7 @@ set (LIBXML2_SUPPORT_DEFAULT ON)
 if (UNIX)
  find_package (LibXml2 MODULE)
  if (LibXml2_FOUND)
+  message (STATUS "found LibXml2")
   set (LIBXML2_FOUND TRUE)
  endif (LibXml2_FOUND)
 elseif (WIN32)
@@ -16,38 +17,45 @@ elseif (WIN32)
   find_package (LibXml2 MODULE)
   find_path (LIBICONV_INCLUDE_DIR NAMES iconv.h
              HINTS "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}"
-             PATH_SUFFIXES include)
+             PATH_SUFFIXES include
+             NO_DEFAULT_PATH)
   if (LibXml2_FOUND)
+   message (STATUS "found LibXml2")
    set (LIBXML2_FOUND TRUE)
-   set (LIBXML2_INCLUDE_DIRS "${LibXml2_INCLUDE_DIRS}")
-   set (LIBXML2_LIBRARIES "${LibXml2_LIBRARIES}")
+   set (LIBXML2_INCLUDE_DIRS "${LIBICONV_INCLUDE_DIR};${LIBXML2_INCLUDE_DIRS}")
    if (CMAKE_BUILD_TYPE STREQUAL "Debug" OR
        CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
-    set (ImageMagick_LIB_DIR "${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/debug/bin")
+    set (LIBXML2_LIB_DIR "${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/debug/bin")
    else ()
-    set (ImageMagick_LIB_DIR "${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/bin")
+    set (LIBXML2_LIB_DIR "${VCPKG_ROOT}/installed/${VCPKG_TARGET_TRIPLET}/bin")
    endif ()
   endif (LibXml2_FOUND)
- else ()
+ endif (VCPKG_SUPPORT)
+ if (NOT LIBXML2_FOUND)
   find_path (LIBICONV_INCLUDE_DIR NAMES iconv.h
-             HINTS "$ENV{LIB_ROOT}/libiconv"
+             PATHS "$ENV{LIB_ROOT}/libiconv"
              PATH_SUFFIXES include)
   find_path (LIBXML2_INCLUDE_DIR NAMES libxml/xpath.h
-             HINTS "$ENV{LIB_ROOT}/libxml2"
+             PATHS "$ENV{LIB_ROOT}/libxml2"
              PATH_SUFFIXES include)
   find_library (LIBICONV_LIBRARY NAMES iconv.lib
-                HINTS "$ENV{LIB_ROOT}/libiconv"
-                PATH_SUFFIXES lib)
+                PATHS "$ENV{LIB_ROOT}/libiconv"
+                PATH_SUFFIXES lib
+                NO_DEFAULT_PATH)
   find_library (LIBXML2_LIBRARY NAMES libxml2.lib
-                HINTS "$ENV{LIB_ROOT}/libxml2"
-                PATH_SUFFIXES lib)
+                PATHS "$ENV{LIB_ROOT}/libxml2"
+                PATH_SUFFIXES lib
+                NO_DEFAULT_PATH)
   if (LIBICONV_LIBRARY AND LIBXML2_LIBRARY)
+   message (STATUS "found LibXml2")
    set (LIBXML2_FOUND TRUE)
    set (LIBXML2_INCLUDE_DIRS "${LIBICONV_INCLUDE_DIR};${LIBXML2_INCLUDE_DIR}")
    set (LIBXML2_LIBRARIES "${LIBICONV_LIBRARY};${LIBXML2_LIBRARY}")
    set (LIBXML2_RUNTIME_LIBDIRS "$ENV{LIB_ROOT}/libiconv/bin;$ENV{LIB_ROOT}/libxml2/bin")
+  else ()
+   message (WARNING "could not found LibXml2, continuing")
   endif (LIBICONV_LIBRARY AND LIBXML2_LIBRARY)
- endif (VCPKG_SUPPORT)
+ endif (NOT LIBXML2_FOUND)
 endif ()
 if (LIBXML2_FOUND)
  option (LIBXML2_SUPPORT "enable libxml2 support" ${LIBXML2_SUPPORT_DEFAULT})
