875e93f2050caf31de8b4cfd2f0cb33fec139a24
Erik Sohns <erik.sohns@posteo.de>
2021-10-09 19:39:13 +0200

    minor changes

--- src/gl/scripts/glm.cmake
+++ src/gl/scripts/glm.cmake
@@ -7,13 +7,30 @@ if (UNIX)
             PATH_SUFFIXES glm
             DOC "searching for glm.h"
             NO_DEFAULT_PATH)
+ get_filename_component (GLM_HPP_PATH ${GLM_HPP_FILE} REALPATH)
+ get_filename_component (GLM_HPP_PATH ${GLM_HPP_PATH} DIRECTORY)
+ get_filename_component (GLM_HPP_PATH ${GLM_HPP_PATH} DIRECTORY)
 elseif (WIN32)
- find_file (GLM_HPP_FILE
-            NAMES "glm.hpp"
-            PATHS $ENV{LIB_ROOT}/glm
-            PATH_SUFFIXES glm
-            DOC "searching for glm.h"
-            NO_DEFAULT_PATH)
+ if (VCPKG_SUPPORT)
+#  cmake_policy (SET CMP0074 OLD)
+  find_package (glm CONFIG)
+  if (glm_FOUND)
+   message (STATUS "found glm")
+   set (GLM_FOUND TRUE)
+   set (GLM_INCLUDE_DIRS "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include")
+  endif (glm_FOUND)
+ endif (VCPKG_SUPPORT)
+ if (NOT GLM_FOUND)
+  find_file (GLM_HPP_FILE
+             NAMES "glm.hpp"
+             PATHS $ENV{LIB_ROOT}/glm
+             PATH_SUFFIXES glm
+             DOC "searching for glm.h"
+             NO_DEFAULT_PATH)
+  get_filename_component (GLM_HPP_PATH ${GLM_HPP_FILE} REALPATH)
+  get_filename_component (GLM_HPP_PATH ${GLM_HPP_PATH} DIRECTORY)
+  get_filename_component (GLM_HPP_PATH ${GLM_HPP_PATH} DIRECTORY)
+ endif (NOT GLM_FOUND)
 endif ()
 if (NOT GLM_HPP_FILE)
  message (WARNING "could not find GLM header, continuing")
@@ -21,9 +38,6 @@ else ()
  message (STATUS "Found GLM header \"${GLM_HPP_FILE}\"")
  option (GLM_SUPPORT "enable glm support" ON)
  if (GLM_SUPPORT)
-  get_filename_component (GLM_HPP_PATH ${GLM_HPP_FILE} REALPATH)
-  get_filename_component (GLM_HPP_PATH ${GLM_HPP_PATH} DIRECTORY)
-  get_filename_component (GLM_HPP_PATH ${GLM_HPP_PATH} DIRECTORY)
   add_definitions (-DGLM_SUPPORT)
  endif (GLM_SUPPORT)
 endif (NOT GLM_HPP_FILE)
