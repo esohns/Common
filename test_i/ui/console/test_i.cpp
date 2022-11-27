424bbaea89be5cf7f8e672ab4e6966826c4d60a2
Erik Sohns <esohns@users.noreply.github.com>
2022-11-27 14:29:59 +0100

    minor changes

--- test_i/ui/console/test_i.cpp
+++ test_i/ui/console/test_i.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 
+#include "unistd.h"
+
 #include <iostream>
 #include <string>
 
@@ -29,10 +31,75 @@
 
 #include "common_test_i_defines.h"
 
+/////////////////////////////////////////
+
+float A, B, C;
+
+float cubeWidth = 20;
+int width = 160, height = 44;
+float zBuffer[160 * 44];
+char buffer[160 * 44];
+int backgroundASCIICode = '.';
+int distanceFromCam = 100;
+float horizontalOffset;
+float K1 = 40;
+
+float incrementSpeed = 0.6f;
+
+float x, y, z;
+float ooz;
+int xp, yp;
+int idx;
+
+float
+calculateX (int i, int j, int k)
+{
+  return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) +
+         j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
+}
+float
+calculateY (int i, int j, int k)
+{
+  return j * cos(A) * cos(C) + k * sin(A) * cos(C) -
+         j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) -
+         i * cos(B) * sin(C);
+}
+float
+calculateZ (int i, int j, int k)
+{
+  return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
+}
+
+void
+calculateForSurface (float cubeX, float cubeY, float cubeZ, int ch)
+{
+  x = calculateX (cubeX, cubeY, cubeZ);
+  y = calculateY (cubeX, cubeY, cubeZ);
+  z = calculateZ (cubeX, cubeY, cubeZ) + distanceFromCam;
+
+  ooz = 1 / z;
+
+  xp = (int)(width / 2 + horizontalOffset + K1 * ooz * x * 2);
+  yp = (int)(height / 2 + K1 * ooz * y);
+
+  idx = xp + yp * width;
+  if (idx >= 0 && idx < width * height)
+  {
+    if (ooz > zBuffer[idx])
+    {
+      zBuffer[idx] = ooz;
+      buffer[idx] = ch;
+    }
+  }
+}
+
+/////////////////////////////////////////
+
 enum Test_I_ModeType
 {
   TEST_I_MODE_FONT_SIZE = 0,
   TEST_I_MODE_WINDOW_SIZE,
+  TEST_I_MODE_CUBE,
   ////////////////////////////////////////
   TEST_I_MODE_MAX,
   TEST_I_MODE_INVALID
@@ -168,6 +235,74 @@ do_work (int argc_in,
 #if defined (ACE_WIN32) || defined (ACE_WIN64)
       struct _SMALL_RECT small_rect_s = Common_UI_Tools::setConsoleMaxWindowSize ();
 #endif // ACE_WIN32 || ACE_WIN64
+
+      break;
+    }
+    case TEST_I_MODE_CUBE:
+    {
+#if defined (ACE_WIN32) || defined (ACE_WIN64)
+      ACE_Time_Value sleep_interval (0, 16000);
+#endif // ACE_WIN32 || ACE_WIN64
+
+      printf ("\x1b[2J");
+      while (1)
+      {
+        memset (buffer, backgroundASCIICode, width * height);
+        memset (zBuffer, 0, width * height * 4);
+        cubeWidth = 20;
+        horizontalOffset = -2 * cubeWidth;
+        // first cube
+        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed)
+          for (float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed)
+          {
+            calculateForSurface (cubeX, cubeY, -cubeWidth, '@');
+            calculateForSurface (cubeWidth, cubeY, cubeX, '$');
+            calculateForSurface (-cubeWidth, cubeY, -cubeX, '~');
+            calculateForSurface (-cubeX, cubeY, cubeWidth, '#');
+            calculateForSurface (cubeX, -cubeWidth, -cubeY, ';');
+            calculateForSurface (cubeX, cubeWidth, cubeY, '+');
+          }
+//        cubeWidth = 10;
+//        horizontalOffset = 1 * cubeWidth;
+//        // second cube
+//        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed)
+//          for (float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed)
+//          {
+//            calculateForSurface (cubeX, cubeY, -cubeWidth, '@');
+//            calculateForSurface (cubeWidth, cubeY, cubeX, '$');
+//            calculateForSurface (-cubeWidth, cubeY, -cubeX, '~');
+//            calculateForSurface (-cubeX, cubeY, cubeWidth, '#');
+//            calculateForSurface (cubeX, -cubeWidth, -cubeY, ';');
+//            calculateForSurface (cubeX, cubeWidth, cubeY, '+');
+//          }
+//        cubeWidth = 5;
+//        horizontalOffset = 8 * cubeWidth;
+//        // third cube
+//        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed)
+//          for (float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed)
+//          {
+//            calculateForSurface (cubeX, cubeY, -cubeWidth, '@');
+//            calculateForSurface (cubeWidth, cubeY, cubeX, '$');
+//            calculateForSurface (-cubeWidth, cubeY, -cubeX, '~');
+//            calculateForSurface (-cubeX, cubeY, cubeWidth, '#');
+//            calculateForSurface (cubeX, -cubeWidth, -cubeY, ';');
+//            calculateForSurface (cubeX, cubeWidth, cubeY, '+');
+//          }
+        printf("\x1b[H");
+        for (int k = 0; k < width * height; k++)
+        {
+          putchar (k % width ? buffer[k] : 10);
+        }
+
+        A += 0.05f;
+        B += 0.05f;
+        C += 0.01f;
+#if defined (ACE_WIN32) || defined (ACE_WIN64)
+        ACE_OS::sleep (sleep_interval);
+#else
+        usleep (8000 * 2);
+#endif // ACE_WIN32 || ACE_WIN64
+      }
 
       break;
     }
