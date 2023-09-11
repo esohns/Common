1db5bf19c48b4f3f1978b5a32829e6b3f5302da4
Erik Sohns <erik.sohns@posteo.de>
2023-09-11 19:45:58 +0200

    minor changes

--- src/math/common_math_tools.h
+++ src/math/common_math_tools.h
@@ -14,7 +14,7 @@ class Common_Math_Tools
  public:
   inline static size_t bits (ACE_UINT32 bitmask_in) { std::bitset<static_cast<size_t> (sizeof (ACE_UINT32) * 8)> bitset_m (static_cast<size_t> (bitmask_in)); return bitset_m.count (); }
   // *IMPORTANT NOTE*: the sign (if any) counts as an extra digit
-  inline static size_t digitsBase10 (int i) { size_t sign_i = (i < 0 ? 1 : 0); return static_cast<int> (::log10 (static_cast<double> (i))) + 1 + sign_i; }
+  inline static size_t digitsBase10 (int i) { size_t sign_i = (i < 0 ? 1 : 0); return static_cast<size_t> (std::log10 (static_cast<double> (i))) + 1 + sign_i; }
 
   static std::string encodeBase64 (const void*,   // data
                                    unsigned int); // length
