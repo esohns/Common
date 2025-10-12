9dd4f2dd44fd6f2f60f07cceccf6fce6b8d151d4
Erik Sohns <erik.sohns@posteo.de>
2025-10-12 20:39:32 +0200

    minor changes

---
+++ test_i/libtorch/test_i.cpp
@@ -0,0 +1,368 @@
+#include "stdafx.h"
+
+//#include "torch/script.h"
+#include "torch/torch.h"
+
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "ace/ACE.h"
+#include "ace/Get_Opt.h"
+#include "ace/High_Res_Timer.h"
+#include "ace/Init_ACE.h"
+#include "ace/OS.h"
+#include "ace/Profile_Timer.h"
+#include "ace/Time_Value.h"
+
+#if defined (HAVE_CONFIG_H)
+#include "Common_config.h"
+#endif // HAVE_CONFIG_H
+
+#include "common_file_tools.h"
+#include "common_string_tools.h"
+
+#include "common_timer_tools.h"
+
+#include "common_log_tools.h"
+
+enum Test_I_ModeType
+{
+  TEST_I_MODE_DEFAULT = 0,
+  TEST_I_MODE_INFERENCE,
+  ////////////////////////////////////////
+  TEST_I_MODE_MAX,
+  TEST_I_MODE_INVALID
+};
+
+template <typename T>
+void
+pretty_print (const std::string& info, T&& data)
+{
+  std::cout << info << std::endl;
+  std::cout << data << std::endl << std::endl;
+}
+
+void
+do_print_usage (const std::string& programName_in)
+{
+  // enable verbatim boolean output
+  std::cout.setf (std::ios::boolalpha);
+
+  std::string path_root =
+    Common_File_Tools::getWorkingDirectory ();
+
+  std::cout << ACE_TEXT_ALWAYS_CHAR ("usage: ")
+            << programName_in
+            << ACE_TEXT_ALWAYS_CHAR (" [OPTIONS]")
+            << std::endl
+            << std::endl;
+  std::cout << ACE_TEXT_ALWAYS_CHAR ("currently available options:")
+            << std::endl;
+  std::cout << ACE_TEXT_ALWAYS_CHAR ("-m          : program mode [")
+            << TEST_I_MODE_DEFAULT
+            << ACE_TEXT_ALWAYS_CHAR ("]")
+            << std::endl;
+  std::cout << ACE_TEXT_ALWAYS_CHAR ("-t          : trace information [")
+            << false
+            << ACE_TEXT_ALWAYS_CHAR ("]")
+            << std::endl;
+}
+
+bool
+do_process_arguments (int argc_in,
+                      ACE_TCHAR** argv_in, // cannot be const...
+                      enum Test_I_ModeType& mode_out,
+                      bool& traceInformation_out)
+{
+  std::string path_root =
+    Common_File_Tools::getWorkingDirectory ();
+
+  // initialize results
+  mode_out = TEST_I_MODE_DEFAULT;
+  traceInformation_out = false;
+
+  ACE_Get_Opt argument_parser (argc_in,
+                               argv_in,
+                               ACE_TEXT ("m:t"),
+                               1,                         // skip command name
+                               1,                         // report parsing errors
+                               ACE_Get_Opt::PERMUTE_ARGS, // ordering
+                               0);                        // for now, don't use long options
+
+  int option = 0;
+  while ((option = argument_parser ()) != EOF)
+  {
+    switch (option)
+    {
+      case 'm':
+      {
+        std::istringstream converter (ACE_TEXT_ALWAYS_CHAR (argument_parser.opt_arg ()),
+                                      std::ios_base::in);
+        int i = 0;
+        converter >> i;
+        mode_out = static_cast<enum Test_I_ModeType> (i);
+        break;
+      }
+      case 't':
+      {
+        traceInformation_out = true;
+        break;
+      }
+      // error handling
+      case ':':
+      {
+        ACE_DEBUG ((LM_ERROR,
+                    ACE_TEXT ("option \"%c\" requires an argument, aborting\n"),
+                    argument_parser.opt_opt ()));
+        return false;
+      }
+      case '?':
+      {
+        ACE_DEBUG ((LM_ERROR,
+                    ACE_TEXT ("unrecognized option \"%s\", aborting\n"),
+                    ACE_TEXT (argument_parser.last_option ())));
+        return false;
+      }
+      case 0:
+      {
+        ACE_DEBUG ((LM_ERROR,
+                    ACE_TEXT ("found long option \"%s\", aborting\n"),
+                    ACE_TEXT (argument_parser.long_option ())));
+        return false;
+      }
+      default:
+      {
+        ACE_DEBUG ((LM_ERROR,
+                    ACE_TEXT ("parse error, aborting\n")));
+        return false;
+      }
+    } // end SWITCH
+  } // end WHILE
+
+  return true;
+}
+
+void
+do_work (int argc_in,
+         ACE_TCHAR* argv_in[],
+         enum Test_I_ModeType mode_in)
+{
+  switch (mode_in)
+  {
+    case TEST_I_MODE_DEFAULT:
+    {
+      torch::Tensor tensor = torch::rand ({2, 3});
+      std::cout << tensor << std::endl;
+
+      break;
+    }
+    case TEST_I_MODE_INFERENCE:
+    {
+      // Use GPU when present, CPU otherwise.
+      torch::Device device (torch::kCPU);
+      if (torch::cuda::is_available ())
+      {
+        device = torch::Device (torch::kCUDA);
+        std::cout << "CUDA is available! Training on GPU." << std::endl;
+      } // end IF
+
+      const size_t kSequenceLen = 1;
+      const size_t kInputDim = 1;
+      const size_t kHiddenDim = 5;
+      const size_t kOuputDim = 1;
+      auto time_serie_detector =
+        torch::nn::LSTM (torch::nn::LSTMOptions (kInputDim, kHiddenDim)
+                           .dropout (0.2)
+                           .num_layers (kSequenceLen)
+                           .bidirectional (false));
+      time_serie_detector->to (device);
+      std::cout << time_serie_detector << std::endl;
+
+      torch::Tensor input = torch::empty ({kSequenceLen, kInputDim});
+      torch::Tensor state = torch::zeros ({2, kSequenceLen, kHiddenDim});
+
+      auto input_acc = input.accessor<float, 2> ();
+      size_t count = 0;
+      for (float i = 0.1f; i < 0.4f; i += 0.1f)
+      {
+        input_acc[count][0] = i;
+        count++;
+      } // end FOR
+      input = input.toBackend (c10::Backend::CPU);
+      state = state.toBackend (c10::Backend::CPU);
+
+      std::cout << "input = " << input << std::endl;
+      time_serie_detector->zero_grad ();
+
+      auto i_tmp = input.view ({input.size (0), 1, -1});
+      auto s_tmp = state.view ({2, state.size (0) / 2, 1, -1});
+      pretty_print ("input: ", i_tmp);
+      pretty_print ("state: ", s_tmp);
+
+      auto rnn_output =
+        time_serie_detector->forward (i_tmp, std::make_tuple (s_tmp[0], s_tmp[1]));
+      pretty_print ("rnn_output/output: ", std::get<0> (rnn_output));
+      pretty_print ("rnn_output/state: ", std::get<0>(std::get<1> (rnn_output)));
+
+      break;
+    }
+    default:
+    {
+      ACE_DEBUG ((LM_ERROR,
+                  ACE_TEXT ("invalid/unknown mode (was: %d), returning\n"),
+                  mode_in));
+      break;
+    }
+  } // end SWITCH
+
+  ACE_DEBUG ((LM_DEBUG,
+              ACE_TEXT ("finished working\n")));
+}
+
+int
+ACE_TMAIN (int argc_in,
+           ACE_TCHAR* argv_in[])
+{
+  int result = EXIT_FAILURE, result_2 = -1;
+
+  // step0: initialize
+  // *PORTABILITY*: on Windows, initialize ACE
+#if defined (ACE_WIN32) || defined (ACE_WIN64)
+  result_2 = ACE::init ();
+  if (result_2 == -1)
+  {
+    ACE_DEBUG ((LM_ERROR,
+                ACE_TEXT ("failed to ACE::init(): \"%m\", aborting\n")));
+    return EXIT_FAILURE;
+  } // end IF
+#endif // ACE_WIN32 || ACE_WIN64
+  ACE_Profile_Timer process_profile;
+  process_profile.start ();
+
+//  Common_Tools::initialize ();
+
+  ACE_High_Res_Timer timer;
+  ACE_Time_Value working_time;
+  ACE_Profile_Timer::ACE_Elapsed_Time elapsed_time;
+  ACE_Profile_Timer::Rusage elapsed_rusage;
+  ACE_Time_Value user_time, system_time;
+
+  // step1a set defaults
+  std::string path_root = Common_File_Tools::getWorkingDirectory ();
+  enum Test_I_ModeType mode_type_e = TEST_I_MODE_DEFAULT;
+  bool trace_information = false;
+  std::string log_file_name;
+
+  // step1b: parse/process/validate configuration
+  if (!do_process_arguments (argc_in,
+                             argv_in,
+                             mode_type_e,
+                             trace_information))
+  {
+    do_print_usage (ACE_TEXT_ALWAYS_CHAR (ACE::basename (argv_in[0], ACE_DIRECTORY_SEPARATOR_CHAR)));
+    goto clean;
+  } // end IF
+
+  //if (!Common_File_Tools::isReadable (source_file_path))
+  //{
+  //  ACE_DEBUG ((LM_ERROR,
+  //              ACE_TEXT ("invalid argument(s), aborting\n")));
+  //  do_print_usage (ACE_TEXT_ALWAYS_CHAR (ACE::basename (argv_in[0], ACE_DIRECTORY_SEPARATOR_CHAR)));
+  //  goto clean;
+  //} // end IF
+
+  // step1c: initialize logging and/or tracing
+  log_file_name =
+    Common_Log_Tools::getLogFilename (ACE_TEXT_ALWAYS_CHAR (Common_PACKAGE_NAME),
+                                      ACE_TEXT_ALWAYS_CHAR (ACE::basename (argv_in[0], ACE_DIRECTORY_SEPARATOR_CHAR)));
+  if (!Common_Log_Tools::initialize (ACE_TEXT_ALWAYS_CHAR (ACE::basename (argv_in[0], ACE_DIRECTORY_SEPARATOR_CHAR)), // program name
+                                     log_file_name,              // log file name
+                                     false,                      // log to syslog ?
+                                     false,                      // trace messages ?
+                                     trace_information,          // debug messages ?
+                                     NULL))                      // logger ?
+  {
+    ACE_DEBUG ((LM_ERROR,
+                ACE_TEXT ("failed to Common_Log_Tools::initialize(), aborting\n")));
+    goto clean;
+  } // end IF
+
+  timer.start ();
+  // step2: do actual work
+  do_work (argc_in,
+           argv_in,
+           mode_type_e);
+  timer.stop ();
+
+  // debug info
+  timer.elapsed_time (working_time);
+  ACE_DEBUG ((LM_INFO,
+              ACE_TEXT ("total working time (h:m:s.us): \"%s\"\n"),
+              ACE_TEXT (Common_Timer_Tools::periodToString (working_time).c_str ())));
+
+  // step3: shut down
+  process_profile.stop ();
+
+  elapsed_time.real_time = 0.0;
+  elapsed_time.user_time = 0.0;
+  elapsed_time.system_time = 0.0;
+  result_2 = process_profile.elapsed_time (elapsed_time);
+  if (result_2 == -1)
+  {
+    ACE_DEBUG ((LM_ERROR,
+                ACE_TEXT ("failed to ACE_Profile_Timer::elapsed_time: \"%m\", aborting\n")));
+    goto clean;
+  } // end IF
+  ACE_OS::memset (&elapsed_rusage, 0, sizeof (ACE_Profile_Timer::Rusage));
+  process_profile.elapsed_rusage (elapsed_rusage);
+  user_time.set (elapsed_rusage.ru_utime);
+  system_time.set (elapsed_rusage.ru_stime);
+#if defined (ACE_WIN32) || defined (ACE_WIN64)
+  ACE_DEBUG ((LM_DEBUG,
+              ACE_TEXT (" --> Process Profile <--\nreal time = %A seconds\nuser time = %A seconds\nsystem time = %A seconds\n --> Resource Usage <--\nuser time used: %s\nsystem time used: %s\n"),
+              elapsed_time.real_time,
+              elapsed_time.user_time,
+              elapsed_time.system_time,
+              ACE_TEXT (Common_Timer_Tools::periodToString (user_time).c_str ()),
+              ACE_TEXT (Common_Timer_Tools::periodToString (system_time).c_str ())));
+#else
+  ACE_DEBUG ((LM_DEBUG,
+              ACE_TEXT (" --> Process Profile <--\nreal time = %A seconds\nuser time = %A seconds\nsystem time = %A seconds\n --> Resource Usage <--\nuser time used: %s\nsystem time used: %s\nmaximum resident set size = %d\nintegral shared memory size = %d\nintegral unshared data size = %d\nintegral unshared stack size = %d\npage reclaims = %d\npage faults = %d\nswaps = %d\nblock input operations = %d\nblock output operations = %d\nmessages sent = %d\nmessages received = %d\nsignals received = %d\nvoluntary context switches = %d\ninvoluntary context switches = %d\n"),
+              elapsed_time.real_time,
+              elapsed_time.user_time,
+              elapsed_time.system_time,
+              ACE_TEXT (Common_Timer_Tools::periodToString (user_time).c_str ()),
+              ACE_TEXT (Common_Timer_Tools::periodToString (system_time).c_str ()),
+              elapsed_rusage.ru_maxrss,
+              elapsed_rusage.ru_ixrss,
+              elapsed_rusage.ru_idrss,
+              elapsed_rusage.ru_isrss,
+              elapsed_rusage.ru_minflt,
+              elapsed_rusage.ru_majflt,
+              elapsed_rusage.ru_nswap,
+              elapsed_rusage.ru_inblock,
+              elapsed_rusage.ru_oublock,
+              elapsed_rusage.ru_msgsnd,
+              elapsed_rusage.ru_msgrcv,
+              elapsed_rusage.ru_nsignals,
+              elapsed_rusage.ru_nvcsw,
+              elapsed_rusage.ru_nivcsw));
+#endif // ACE_WIN32 || ACE_WIN64
+
+  result = EXIT_SUCCESS;
+
+clean:
+  Common_Log_Tools::finalize ();
+  // *PORTABILITY*: on Windows, finalize ACE
+#if defined (ACE_WIN32) || defined (ACE_WIN64)
+  result = ACE::fini ();
+  if (result == -1)
+    ACE_DEBUG ((LM_ERROR,
+                ACE_TEXT ("failed to ACE::fini(): \"%m\", continuing\n")));
+#endif // ACE_WIN32 || ACE_WIN64
+
+  return result;
+}
