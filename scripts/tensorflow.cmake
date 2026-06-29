d3ea5cac62446d17f187162447ed1036669f5f6a
Erik Sohns <erik.sohns@posteo.de>
2026-06-29 20:39:36 +0200

    minor changes

--- scripts/tensorflow.cmake
+++ scripts/tensorflow.cmake
@@ -97,22 +97,7 @@ elseif (WIN32)
    set (tensorflow_LIB_DIR "${TENSORFLOW_ROOT}/lib")
   endif (TENSORFLOW_LIBRARY)
  endif (NOT TENSORFLOW_FOUND)
- 
- # if (EXISTS $ENV{TENSORFLOW_CC_ROOT})
- #  set (TENSORFLOW_CC_ROOT $ENV{TENSORFLOW_CC_ROOT})
- # else ()
- #  set (TENSORFLOW_CC_ROOT "C:/tf")
- # endif (EXISTS $ENV{TENSORFLOW_CC_ROOT})
- # find_library (TENSORFLOW_CC_LIBRARY libtensorflow_cc.so.2.15.0.if.lib
- #               PATHS ${TENSORFLOW_CC_ROOT}/bazel-out/x64_windows-opt/bin
- #               PATH_SUFFIXES tensorflow
- #               DOC "searching for libtensorflow_cc.so.2.15.0.if.lib"
- #               NO_DEFAULT_PATH)
-#  if (NOT TENSORFLOW_CC_LIBRARY)
-#   message (WARNING "could not find libtensorflow_cc.so.2.15.0.if.lib, continuing")
-#  else ()
-#   message (STATUS "Found libtensorflow_cc.so.2.15.0.if.lib library \"${TENSORFLOW_CC_LIBRARY}\"")
-#  endif (NOT TENSORFLOW_CC_LIBRARY)
+
  find_library (TENSORFLOW_CC_LIBRARY tensorflow_cc.lib
                PATHS $ENV{LIB_ROOT}
                PATH_SUFFIXES tensorflow_cc
@@ -124,9 +109,9 @@ elseif (WIN32)
   message (STATUS "Found tensorflow_cc.lib library \"${TENSORFLOW_CC_LIBRARY}\"")
  endif (NOT TENSORFLOW_CC_LIBRARY)
  if (TENSORFLOW_CC_LIBRARY)
-#*NOTE*: tensorflow_cc.dll is missing some references on Windows, so tensorflow_cc support is disabled for now
-#  set (TENSORFLOW_CC_FOUND TRUE)
+  set (TENSORFLOW_CC_FOUND TRUE)
   set (tensorflow_cc_LIBRARIES "${TENSORFLOW_CC_LIBRARY}")
+#*WARNING*: the includes for Tensorflow C API conflict with the ones from the Tensorflow C++ API... :-(
   set (tensorflow_cc_INCLUDE_DIRS "$ENV{LIB_ROOT}/tensorflow_cc/include;$ENV{LIB_ROOT}/tensorflow_cc/include/src")
   set (tensorflow_cc_LIB_DIR "$ENV{LIB_ROOT}/tensorflow_cc")
  endif (TENSORFLOW_CC_LIBRARY)
@@ -135,13 +120,11 @@ if (TENSORFLOW_FOUND)
  option (TENSORFLOW_SUPPORT "enable tensorflow support" ${TENSORFLOW_SUPPORT_DEFAULT})
  if (TENSORFLOW_SUPPORT)
   add_definitions (-DTENSORFLOW_SUPPORT)
-#  include_directories (${tensorflow_INCLUDE_DIRS})
  endif (TENSORFLOW_SUPPORT)
 endif (TENSORFLOW_FOUND)
 if (TENSORFLOW_CC_FOUND)
  option (TENSORFLOW_CC_SUPPORT "enable tensorflow C++ support" ${TENSORFLOW_CC_SUPPORT_DEFAULT})
  if (TENSORFLOW_CC_SUPPORT)
   add_definitions (-DTENSORFLOW_CC_SUPPORT)
-#  include_directories (${tensorflow_cc_INCLUDE_DIRS})
  endif (TENSORFLOW_CC_SUPPORT)
 endif (TENSORFLOW_CC_FOUND)
